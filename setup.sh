91bb558f6d42aa3ad6f5afb9c84a89c9ec544755
Ben Booth <bkbooth@gmail.com>
2017-06-26 23:57:19 +1000

    Check for and install Vundle + dependencies

--- setup.sh
+++ setup.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
-echo "setting up..."
-echo "done."
+# Change to the directory we're executing from
+cd `dirname $0`
+
+# Link all dotfiles
+echo "Linking dotfiles..."
+for file in `ls .`; do
+  # Skip .md and .sh files, and directories
+  if [[ $file =~ \.(md|sh) ]] || [[ -d $file ]]; then
+    echo "Skipping '$file'"
+    continue
+  fi
+
+  echo "Linking '.$file'"
+  ln -snf "$PWD/$file" "$HOME/.$file"
+done
+echo "Done."
+
+# Check for and install Vundle
+echo "Setting up Vundle..."
+if ! [[ -d ~/.vim/bundle/Vundle.vim ]]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+# Install/update Vundle dependencies
+vim -c VundleUpdate -c quitall
